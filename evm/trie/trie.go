e7171d489b6b1253ec041463c860720f220d32f0
steviezhang <stephanie@chainsafe.io>
2020-07-01 17:36:43 -0400

    more fixes

--- evm/trie/trie.go
+++ evm/trie/trie.go
@@ -5,6 +5,7 @@ package trie
 
 import (
 
+	"errors"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
@@ -15,33 +16,47 @@ type Trie struct {
 	trieRoots []common.Hash
 }
 
+var (
+	emptyRoot = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
+)
+
 func NewTrie(root common.Hash, db *Database) (*Trie, error) {
 
 	// TODO: look into cache values
 	// this creates a new trie database with our KVDB as the diskDB for node storage
 	newTrie, err := trie.New(root, trie.NewDatabaseWithCache(db, 0))
 
-	trie := &Trie{
+	if err != nil {
+		return nil, err
+	}
 
+	trie := &Trie{
 		t: newTrie,
 	}
 
-	if root != (common.Hash{}) && root != trie.emptyRoot {
+	if root != (common.Hash{}) && root != emptyRoot {
 		trie.trieRoots = []common.Hash{root}
 	}
 
-	return trie
+	return trie, nil
 }
 
-func (*Trie) UpdateTrie(transactions []common.Hash, transactionRoot common.Hash) {
+func (t *Trie) UpdateTrie(transactions []common.Hash, transactionRoot common.Hash) error {
 	for i, tx := range transactions {
-		key = rlp.EncodeToBytes(i)
-		Trie.t.Update(key, tx)
+		key, err := rlp.EncodeToBytes(i)
+		if err != nil {
+			return err
+		}
+
+		t.t.Update(key, tx)
 	}
 
-	require(t.root == transactionRoot, "transaction roots don't match")
+	if *Trie.t.root != transactionRoot{
+		return errors.New("transaction roots don't match")
+	}
 
-	Trie.trieRoots = append(Trie.trieRoots, transactionRoot)
+	*Trie.trieRoots = append(Trie.trieRoots, transactionRoot)
+	return nil
 }
 
 // retrieves a proof from a trie object, given the root of the trie it is contained in and the key
