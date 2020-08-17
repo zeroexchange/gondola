64514e9d095f03b8c0e1be9c06b7781fc6caad67
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-17 16:25:42 +0000

    Deploy ChainSafe/ChainBridge to github.com/ChainSafe/ChainBridge.git:gh-pages

---
+++ core/chain.go
@@ -0,0 +1,30 @@
+// Copyright 2020 ChainSafe Systems
+// SPDX-License-Identifier: LGPL-3.0-only
+
+package core
+
+import (
+	msg "github.com/ChainSafe/ChainBridge/message"
+	"github.com/ChainSafe/ChainBridge/router"
+)
+
+type Chain interface {
+	Start() error // Start chain
+	SetRouter(*router.Router)
+	Id() msg.ChainId
+	Name() string
+	Stop()
+}
+
+type ChainConfig struct {
+	Name           string            // Human-readable chain name
+	Id             msg.ChainId       // ChainID
+	Endpoint       string            // url for rpc endpoint
+	From           string            // address of key to use
+	KeystorePath   string            // Location of key files
+	Insecure       bool              // Indicated whether the test keyring should be used
+	BlockstorePath string            // Location of blockstore
+	FreshStart     bool              // If true, blockstore is ignored at start.
+	LatestBlock    bool              // If true, overrides blockstore or latest block in config and starts from current block
+	Opts           map[string]string // Per chain options
+}
